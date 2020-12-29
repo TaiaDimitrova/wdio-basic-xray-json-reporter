cc333f03db4fdd553315632e39f3c612116c8bda
Victor JEMAN <victorjeman@gmail.com>
2020-12-29 13:56:21 +0200

    Update

--- src/mergeResults.js
+++ src/mergeResults.js
@@ -4,20 +4,20 @@ const path = require("path");
 function writeFile(dir, mergedResults, customFileName) {
     let fileName = customFileName || "wdio-merged.json";
     const filePath = path.join(dir, fileName);
-    console.log('FILEPATH: ', filePath)
-    console.log('DIR: ', dir)
-    console.log('mergedResults: ', mergedResults)
-    console.log('customFileName: ', customFileName)
+    console.log("FILEPATH: ", filePath);
+    console.log("DIR: ", dir);
+    console.log("mergedResults: ", mergedResults);
+    console.log("customFileName: ", customFileName);
     try {
         fs.writeFileSync(filePath, JSON.stringify(mergedResults));
     } catch (error) {
-        console.log('ERROR on write in writeFile: ', error);
+        console.log("ERROR on write in writeFile: ", error);
     }
-    
 }
-function hello(){
-    console.log('Hello guys!');
+function hello() {
+    console.log("Hello guys!");
 }
+
 function getDataFromFiles(dir, filePattern) {
     const fileNames = fs
         .readdirSync(dir)
@@ -26,15 +26,14 @@ function getDataFromFiles(dir, filePattern) {
 
     fileNames.forEach((fileName) => {
         let fileContent = "";
-        
+
         try {
             fileContent = fs.readFileSync(`${dir}/${fileName}`);
             const fileContentParsed = JSON.parse(fileContent);
             data.push(JSON.parse(fileContentParsed));
         } catch (error) {
-            console.log('JSON parse failed with error: ', error)
+            console.log("JSON parse failed with error: ", error);
         }
-        
     });
 
     return data;
@@ -43,7 +42,7 @@ function getDataFromFiles(dir, filePattern) {
 function mergeData(rawData) {
     let mergedResults;
 
-    console.log('RAW DATA: ',rawData);
+    console.log("RAW DATA: ", rawData);
     rawData.forEach((data) => {
         if (mergedResults === undefined) {
             // use the first result so that we have the right shape
@@ -61,7 +60,7 @@ function mergeData(rawData) {
 
     mergedResults.tests.sort((a, b) => (a.status < b.status ? 1 : -1));
 
-    console.log('MERGED result set: ', mergedResults);
+    console.log("MERGED result set: ", mergedResults);
     return mergedResults;
 }
 
@@ -74,8 +73,6 @@ const mergeResults = (...args) => {
     const mergedResults = mergeData(rawData);
     hello();
     writeFile(dir, mergedResults, customFileName);
-    
 };
- 
 
 module.exports = mergeResults;
